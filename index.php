f1ced8e0fa4122dd3a803008ca17b4e56b429ebb
Merili <meriliasu@gmail.com>
2014-09-09 16:01:32 +0300

    Add my name variables
    Add my name variables

--- index.php
+++ index.php
@@ -28,6 +28,32 @@
 	    echo "<br>";
 	   	echo ($muutuja1 . $muutuja2);
 	?>
+
+	<h2>Nimekirja väljastamine muutujate ja stringide abil</h2>
+	<?php
+	    $student1 = "Fränk";
+	    $student2 = "Syret";
+	    $student3 = "Triinu";
+	    $student4 = "Silja";
+	    $student5 = "Anneli";
+	?>
+	<ul>
+	<?php
+	echo
+	 		"<li>$student1</li>;
+	 		<li>$student2</li>;
+	 		<li>$student3</li>;
+	 		<li>$student4</li>;
+	 		<li>$student5</li>;"
+	?>
+	</ul>
+	 <?php
+	    $firstName = "Fränk";
+	    $lastName = "Ainla";
+	    $fullName = ("$firstName " . $lastName);
+
+	    echo $fullName;
+	?>
 </body>
 </html>
 	
